5a863f30543e154d4d90caef846a7ab4825960e9
Abdullah <abdullahhamednew@gmail.com>
2021-10-30 01:03:51 +0200

    Creation du chateau avec les fonctions

--- examples/webgl.js
+++ examples/webgl.js
@@ -45,6 +45,91 @@ function init() {
 
     creation.createCylinder([0,0,0], [20,20,20], ['texture_wall'], scene);
 
+//Les Cones
+
+    creation.createCone([175, 210, 175], [75, 100, 70], ['texture_cone'], scene);
+
+    creation.createCone([-190, 125, -190], [50, 65, 50], ['texture_cone'], scene);
+
+    creation.createCone([190, 125, -190], [50, 65, 50], ['texture_cone'], scene);
+
+    creation.createCone([175, 210, 175], [75, 100, 70], ['texture_cone'], scene);
+
+
+//Les Cylindres
+
+    creation.createCylinder([175, 35, 175], [75, 75, 250, 100], ['texture_wall2'], scene);
+
+    creation.createCylinder([-175, 30, 175], [70, 70, 230, 100], ['texture_wall2'], scene);
+
+    creation.createCylinder([-175, 150, 175], [85, 85, 40, 100], ['texture_wall2'], scene);
+
+    creation.createCylinder([190, 5, -190], [50, 50, 175, 100], ['texture_wall2'], scene);
+
+    creation.createCylinder([-190, 5, -190], [50, 50, 175, 100], ['texture_wall2'], scene);
+
+//Bases tours milieu
+
+    creation.createCylinder([-57, -10, -200], [25, 25, 120, 100], ['texture_wall2'], scene);
+
+    creation.createCylinder([57, -10, -200], [25, 25, 120, 100], ['texture_wall2'], scene);
+
+
+//LES WALLS
+
+//Mur arrière
+
+    creation.createBox([0, -5, 200], [400, 125, 80], ['texture_wall2'], scene);
+
+//Mur avant
+
+    creation.createBox([137.5, -15, -200], [150, 100, 20], ['texture_wall2'], scene);
+
+    creation.createBox([-137.5, -15, -200], [150, 100, 20], ['texture_wall2'], scene);
+
+
+//Pont-levis
+
+    creation.createBox([0, -45, -260], [65, 5, 120], ['texture_wood'], scene);
+
+
+//Mur droit
+
+    creation.createBox([-200, -5, 0], [20, 125, 400], ['texture_wall2'], scene);
+
+
+//Mur gauche
+
+    creation.createBox([200, -5, 0], [20, 125, 400], ['texture_wall2'], scene);
+
+// CHAMBRE
+
+
+
+//LE FLOOR (le sol)
+
+    creation.createBox([0, -65, 0], [400, 40, 400], ['texture_dirt'], scene);
+
+
+//WATER
+
+    creation.createBox([0, -70, 0], [600, 10, 610], ['texture_water'], scene);
+
+
+//Grass
+
+    creation.createBox([0, -70, -450], [1200, 40, 300], ['texture_grass'], scene);
+
+    creation.createBox([0, -70, 450], [1200, 40, 300], ['texture_grass'], scene);
+
+    creation.createBox([-450, -70, 0], [300, 40, 1200], ['texture_grass'], scene);
+
+    creation.createBox([450, -70, 0], [300, 40, 1200], ['texture_grass'], scene);
+
+
+
+
+
 }
 
 function onWindowResize() {
