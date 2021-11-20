4c17da9d7524fe52f7651474f2327084b832055a
Abdullah <abdullahhamednew@gmail.com>
2021-11-20 17:02:07 +0100

    Modification du desing, fixed camera, et remove bugs

--- examples/webgl.js
+++ examples/webgl.js
@@ -2,14 +2,7 @@ import * as THREE from '../build/three.module.js';
 import { OrbitControls } from './jsm/controls/OrbitControls.js';
 import * as creation from './Fonctions.js';
 
-let camera, scene, renderer;
-let cone_arG, cone_arD, cone_avD, cone_avG, cone_avMG, cone_avMD;
-let floor;
-let cylinder_arG, cylinder_arD, cylinder_avG, cylinder_avD, cylinder_arDbis, cylinder_avMD, cylinder_avMG;
-let wall_ar, wall_avG, wall_avD, pont_levis, wall_D, wall_G, wall5, wall6, wall7, wall_avM;
-let water;
-let grass_av, grass_ar, grass_D, grass_G;
-let controls;
+let camera, scene, renderer, controls;
 
 init();
 animate();
@@ -38,6 +31,8 @@ function init() {
 ************************/
     controls = new OrbitControls(camera, renderer.domElement);
     window.addEventListener( 'resize', onWindowResize );
+    controls.maxPolarAngle = Math.PI / 2;
+    controls.minPolarAngle = Math.PI / 4;
 
 
 
@@ -53,12 +48,12 @@ function init() {
 
   // Tower behind right
   creation.createCylinder([-175, 30, 175], [70, 70, 230, 100], ['texture_wall2'], scene); // Base
-  creation.createCylinder([-175, 150, 175], [85, 85, 40, 100], ['texture_wall2'], scene); // Haut de la tour
+  creation.createCylinder([-175, 150, 175], [85, 85, 40, 100], ['texture_wall2'], scene); // Top of tower
 
   // Tower front right
   creation.createCylinder([-190, 5, -190], [50, 50, 175, 100], ['texture_wall2'], scene);
   creation.createCone([-190, 125, -190], [50, 65, 50], ['texture_cone'], scene);
-  creation.createCylinder([-155, 55, -160], [30, 30, 7.5, 50], ['texture_wood'], scene); // Zone pour marcher
+  creation.createCylinder([-155, 55, -160], [30, 30, 7.5, 50], ['texture_wood'], scene); // Walk zone
 
   // Tower front middle right
   creation.createCylinder([-57, 0, -200], [25, 25, 170, 100], ['texture_wall2'], scene);
@@ -71,7 +66,7 @@ function init() {
   // Tower front left
   creation.createCylinder([190, 5, -190], [50, 50, 175, 100], ['texture_wall2'], scene);
   creation.createCone([190, 125, -190], [50, 65, 50], ['texture_cone'], scene);
-  creation.createCylinder([155, 55, -160], [30, 30, 7.5, 50], ['texture_wood'], scene); // Zone pour marcher
+  creation.createCylinder([155, 55, -160], [30, 30, 7.5, 50], ['texture_wood'], scene); // Walk zone
 
 
 
@@ -79,12 +74,12 @@ function init() {
 // Walls
 
   // Wall behind
-  creation.createBox([10, -5, 150], [270, 120, 110], ['texture_wall2'], scene);
+  creation.createBox([10, 20, 195], [270, 170, 50], ['texture_wall2'], scene);
 
   // Wall left
   creation.createBox([195, 0, 0], [20, 140, 400], ['texture_wall2'], scene);
-  creation.createBox([170, 55, -70], [30, 7.5, 165], ['texture_wood'], scene); // Zone pour marcher
-  //Boucle remparts wall left
+  creation.createBox([170, 55, -70], [30, 7.5, 165], ['texture_wood'], scene); // Walk zone
+  // Loop remparts wall left
    let z = 32;
    for (var i = 0; i < 6; i++) {
    creation.createBox([195, 75, z], [20, 10, 15], ['texture_wall2'], scene);
@@ -93,8 +88,8 @@ function init() {
 
   // Wall front left
   creation.createBox([137.5, 0, -200], [150, 140, 20], ['texture_wall2'], scene);
-  creation.createBox([105, 55, -175], [140, 7.5, 30], ['texture_wood'], scene); // Zone pour marcher
-  //Boucle remparts wall front left
+  creation.createBox([105, 55, -175], [140, 7.5, 30], ['texture_wood'], scene); // Walk zone
+  // Loop remparts wall front left
    let x = 125;
    for (var i = 0; i < 2; i++) {
    creation.createBox([x, 75, -200], [15, 10, 20], ['texture_wall2'], scene);
@@ -103,12 +98,12 @@ function init() {
 
   // Drawbridge
   creation.createBox([0, 55, -200], [80, 20, 20], ['texture_wall2'], scene);
-  creation.createBox([0, -45, -252.5], [65, 5, 105], ['texture_wood'], scene); // Mur au-dessus
+  creation.createBox([0, -45, -252.5], [65, 5, 105], ['texture_wood'], scene); // Connexion bar
 
   // Wall front right
   creation.createBox([-137.5, 0, -200], [150, 140, 20], ['texture_wall2'], scene);
-  creation.createBox([-105, 55, -175], [140, 7.5, 30], ['texture_wood'], scene); // Zone pour marcher
-  //Boucle remparts wall front right
+  creation.createBox([-105, 55, -175], [140, 7.5, 30], ['texture_wood'], scene); // Walk zone
+  // Loop remparts wall front right
    let x2 = -125;
    for (var i = 0; i < 2; i++) {
    creation.createBox([x2, 75, -200], [15, 10, 20], ['texture_wall2'], scene);
@@ -117,8 +112,8 @@ function init() {
 
   // Wall right
   creation.createBox([-195, 0, 0], [20, 140, 400], ['texture_wall2'], scene);
-  creation.createBox([-170, 55, -10], [30, 7.5, 300], ['texture_wood'], scene); // Zone pour marcher
-  //Boucle remparts wall right
+  creation.createBox([-170, 55, -10], [30, 7.5, 300], ['texture_wood'], scene); // Walk zone
+  // Loop remparts wall right
    let z4 = 90;
    for (var i = 0; i < 8; i++) {
    creation.createBox([-195, 75, z4], [20, 10, 15], ['texture_wall2'], scene);
@@ -141,6 +136,8 @@ function init() {
   creation.createBox([-450, -70, 0], [300, 40, 1200], ['texture_grass'], scene);
 
   creation.createBox([450, -70, 0], [300, 40, 1200], ['texture_grass'], scene);
+
+  creation.createBox([0, -85, 0], [900, 10, 900], ['texture_grass'], scene);
 
 // Stairs
      let y = -50;
