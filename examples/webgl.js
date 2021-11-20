56575252284babe3ffb0aedfe50b6dea2822d876
Kevin Kusmider <kevin.kusmider@hotmail.fr>
2021-11-20 22:33:21 +0100

    Ajout pont levis qui bouge

--- examples/webgl.js
+++ examples/webgl.js
@@ -2,14 +2,12 @@ import * as THREE from '../build/three.module.js';
 import { OrbitControls } from './jsm/controls/OrbitControls.js';
 import * as creation from './Fonctions.js';
 
-let camera, scene, renderer;
-let cone_arG, cone_arD, cone_avD, cone_avG, cone_avMG, cone_avMD;
-let floor;
-let cylinder_arG, cylinder_arD, cylinder_avG, cylinder_avD, cylinder_arDbis, cylinder_avMD, cylinder_avMG;
-let wall_ar, wall_avG, wall_avD, pont_levis, wall_D, wall_G, wall5, wall6, wall7, wall_avM;
-let water;
-let grass_av, grass_ar, grass_D, grass_G;
-let controls;
+let camera, scene, renderer, controls;
+
+let x, y, z;
+
+let click, raycaster, pivot;
+let elements = {};
 
 init();
 animate();
@@ -19,27 +17,35 @@ function init() {
 /**********************
     INITIALISATIONS
 ************************/
-//Initialisation de la caméra
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 2000 );
+// Initialisation camera
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 2000);
     camera.position.z = -600;
     camera.position.y = 100;
 
-// Initialisation de la scène
+// Initialisation scene
     scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xbfd1e5);
 
-// Initialisation du renderer
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+// Initialisation renderer
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+// Initialisation Raycaster
+    click = new THREE.Vector2();
+    raycaster = new THREE.Raycaster();
+
+// Event Listenners
+    window.addEventListener('click', onClick); // When click
+    window.addEventListener('resize', onWindowResize); // When window's resized
 
 /**********************
     CONTROLS
 ************************/
     controls = new OrbitControls(camera, renderer.domElement);
-    window.addEventListener( 'resize', onWindowResize );
-
-
+    controls.maxPolarAngle = Math.PI / 2;
+    controls.update();
 
 /**********************
     BUILDING
@@ -47,115 +53,127 @@ function init() {
 
 // Towers
 
-  // Tower behind left
-  creation.createCylinder([150, 65, 150], [100, 100, 300, 100], ['texture_wall2'], scene);
-  creation.createCone([150, 275, 150], [100, 120, 100], ['texture_cone'], scene);
+    // Tower behind left
+    creation.createCylinder([150, 65, 150], [100, 100, 300, 100], ['texture_wall2'], scene);
+    creation.createCone([150, 275, 150], [100, 120, 100], ['texture_cone'], scene);
 
-  // Tower behind right
-  creation.createCylinder([-175, 30, 175], [70, 70, 230, 100], ['texture_wall2'], scene); // Base
-  creation.createCylinder([-175, 150, 175], [85, 85, 40, 100], ['texture_wall2'], scene); // Haut de la tour
+    // Tower behind right
+    creation.createCylinder([-175, 30, 175], [70, 70, 230, 100], ['texture_wall2'], scene); // Base
+    creation.createCylinder([-175, 150, 175], [85, 85, 40, 100], ['texture_wall2'], scene); // Haut de la tour
 
-  // Tower front right
-  creation.createCylinder([-190, 5, -190], [50, 50, 175, 100], ['texture_wall2'], scene);
-  creation.createCone([-190, 125, -190], [50, 65, 50], ['texture_cone'], scene);
-  creation.createCylinder([-155, 55, -160], [30, 30, 7.5, 50], ['texture_wood'], scene); // Zone pour marcher
+    // Tower front right
+    creation.createCylinder([-190, 5, -190], [50, 50, 175, 100], ['texture_wall2'], scene);
+    creation.createCone([-190, 125, -190], [50, 65, 50], ['texture_cone'], scene);
+    creation.createCylinder([-155, 55, -160], [30, 30, 7.5, 50], ['texture_wood'], scene); // Zone pour marcher
 
-  // Tower front middle right
-  creation.createCylinder([-57, 0, -200], [25, 25, 170, 100], ['texture_wall2'], scene);
-  creation.createCylinder([-57, 87.5, -200], [30, 30, 20, 100], ['texture_wall2'], scene);
+    // Tower front middle right
+    creation.createCylinder([-57, 0, -200], [25, 25, 170, 100], ['texture_wall2'], scene);
+    creation.createCylinder([-57, 87.5, -200], [30, 30, 20, 100], ['texture_wall2'], scene);
 
-  // Tower front middle left
-  creation.createCylinder([57, 0, -200], [25, 25, 170, 100], ['texture_wall2'], scene);
-  creation.createCylinder([57, 87.5, -200], [30, 30, 20, 100], ['texture_wall2'], scene);
+    // Tower front middle left
+    creation.createCylinder([57, 0, -200], [25, 25, 170, 100], ['texture_wall2'], scene);
+    creation.createCylinder([57, 87.5, -200], [30, 30, 20, 100], ['texture_wall2'], scene);
 
-  // Tower front left
-  creation.createCylinder([190, 5, -190], [50, 50, 175, 100], ['texture_wall2'], scene);
-  creation.createCone([190, 125, -190], [50, 65, 50], ['texture_cone'], scene);
-  creation.createCylinder([155, 55, -160], [30, 30, 7.5, 50], ['texture_wood'], scene); // Zone pour marcher
+    // Tower front left
+    creation.createCylinder([190, 5, -190], [50, 50, 175, 100], ['texture_wall2'], scene);
+    creation.createCone([190, 125, -190], [50, 65, 50], ['texture_cone'], scene);
+    creation.createCylinder([155, 55, -160], [30, 30, 7.5, 50], ['texture_wood'], scene); // Zone pour marcher
 
 
 
 
 // Walls
 
-  // Wall behind
-  creation.createBox([10, -5, 150], [270, 120, 110], ['texture_wall2'], scene);
+    // Wall behind
+    creation.createBox([10, -5, 150], [270, 120, 110], ['texture_wall2'], scene);
 
-  // Wall left
-  creation.createBox([195, 0, 0], [20, 140, 400], ['texture_wall2'], scene);
-  creation.createBox([170, 55, -70], [30, 7.5, 165], ['texture_wood'], scene); // Zone pour marcher
-  //Boucle remparts wall left
-   let z = 32;
-   for (var i = 0; i < 6; i++) {
-   creation.createBox([195, 75, z], [20, 10, 15], ['texture_wall2'], scene);
-   z = z - 30;
-   }
+    // Wall left
+    creation.createBox([195, 0, 0], [20, 140, 400], ['texture_wall2'], scene);
+    creation.createBox([170, 55, -70], [30, 7.5, 165], ['texture_wood'], scene); // Zone pour marcher
 
-  // Wall front left
-  creation.createBox([137.5, 0, -200], [150, 140, 20], ['texture_wall2'], scene);
-  creation.createBox([105, 55, -175], [140, 7.5, 30], ['texture_wood'], scene); // Zone pour marcher
-  //Boucle remparts wall front left
-   let x = 125;
-   for (var i = 0; i < 2; i++) {
-   creation.createBox([x, 75, -200], [15, 10, 20], ['texture_wall2'], scene);
-   x = x - 25;
-   }
+    //Boucle remparts wall left
+    z = 32;
+    for (var i = 0; i < 6; i++) {
+        creation.createBox([195, 75, z], [20, 10, 15], ['texture_wall2'], scene);
+        z = z - 30;
+    }
 
-  // Drawbridge
-  creation.createBox([0, 55, -200], [80, 20, 20], ['texture_wall2'], scene);
-  creation.createBox([0, -45, -252.5], [65, 5, 105], ['texture_wood'], scene); // Mur au-dessus
+    // Wall front left
+    creation.createBox([137.5, 0, -200], [150, 140, 20], ['texture_wall2'], scene);
+    creation.createBox([105, 55, -175], [140, 7.5, 30], ['texture_wood'], scene); // Zone pour marcher
 
-  // Wall front right
-  creation.createBox([-137.5, 0, -200], [150, 140, 20], ['texture_wall2'], scene);
-  creation.createBox([-105, 55, -175], [140, 7.5, 30], ['texture_wood'], scene); // Zone pour marcher
-  //Boucle remparts wall front right
-   let x2 = -125;
-   for (var i = 0; i < 2; i++) {
-   creation.createBox([x2, 75, -200], [15, 10, 20], ['texture_wall2'], scene);
-   x2 = x2 + 25;
-   }
+    //Boucle remparts wall front left
+    x = 125;
+    for (var i = 0; i < 2; i++) {
+        creation.createBox([x, 75, -200], [15, 10, 20], ['texture_wall2'], scene);
+        x = x - 25;
+    }
 
-  // Wall right
-  creation.createBox([-195, 0, 0], [20, 140, 400], ['texture_wall2'], scene);
-  creation.createBox([-170, 55, -10], [30, 7.5, 300], ['texture_wood'], scene); // Zone pour marcher
-  //Boucle remparts wall right
-   let z4 = 90;
-   for (var i = 0; i < 8; i++) {
-   creation.createBox([-195, 75, z4], [20, 10, 15], ['texture_wall2'], scene);
-   z4 = z4 - 30;
-   }
+    // Drawbridge
+    creation.createBox([0, 55, -200], [80, 20, 20], ['texture_wall2'], scene); // Mur au-dessus
+    creation.createBox([0, -55, -210], [80, 20, 20], ['texture_wall2'], scene); // Mur en-dessous
+    pivot = new THREE.Group();
+    pivot.position.set( 32, -45, -200);
+    let bridge = creation.createBox([-32, 0, -52.5], [65, 5, 105], ['texture_wood'])
+    bridge.userData.draggable = true;
+    bridge.userData.name = "bridge";
+    pivot.userData.status = "down";
+    pivot.add(bridge);
+    scene.add(pivot);
+    elements['bridge'] = pivot;
+
+    // Wall front right
+    creation.createBox([-137.5, 0, -200], [150, 140, 20], ['texture_wall2'], scene);
+    creation.createBox([-105, 55, -175], [140, 7.5, 30], ['texture_wood'], scene); // Zone pour marcher
+    //Boucle remparts wall front right
+    x = -125;
+    for (var i = 0; i < 2; i++) {
+        creation.createBox([x, 75, -200], [15, 10, 20], ['texture_wall2'], scene);
+        x = x + 25;
+    }
+
+    // Wall right
+    creation.createBox([-195, 0, 0], [20, 140, 400], ['texture_wall2'], scene);
+    creation.createBox([-170, 55, -10], [30, 7.5, 300], ['texture_wood'], scene); // Zone pour marcher
+
+    //Boucle remparts wall right
+    z = 90;
+    for (var i = 0; i < 8; i++) {
+        creation.createBox([-195, 75, z], [20, 10, 15], ['texture_wall2'], scene);
+        z = z - 30;
+    }
 
 // Floor
 
-  // Floor inside castle
-  creation.createBox([0, -65, 0], [400, 40, 400], ['texture_dirt'], scene);
+    // Floor inside castle
+    creation.createBox([0, -65, 0], [400, 40, 400], ['texture_dirt'], scene);
 
-  // Water
-  creation.createBox([0, -70, 0], [600, 10, 610], ['texture_water'], scene);
+    // Water
+    creation.createBox([0, -70, 0], [600, 10, 610], ['texture_water'], scene);
 
-  // Grass
-  creation.createBox([0, -70, -450], [1200, 40, 300], ['texture_grass'], scene);
+    // Grass
+    creation.createBox([0, -70, -450], [1200, 40, 300], ['texture_grass'], scene);
 
-  creation.createBox([0, -70, 450], [1200, 40, 300], ['texture_grass'], scene);
+    creation.createBox([0, -70, 450], [1200, 40, 300], ['texture_grass'], scene);
 
-  creation.createBox([-450, -70, 0], [300, 40, 1200], ['texture_grass'], scene);
+    creation.createBox([-450, -70, 0], [300, 40, 1200], ['texture_grass'], scene);
 
-  creation.createBox([450, -70, 0], [300, 40, 1200], ['texture_grass'], scene);
+    creation.createBox([450, -70, 0], [300, 40, 1200], ['texture_grass'], scene);
 
 // Stairs
-     let y = -50;
-     let x3 = 76;
-     for (var i = 0; i < 14; i++) {
-       creation.createBox([x3,y,44], [10,7.5,65], ['texture_wall2'], scene );
-       y = y + 7.5;
-       x3 = x3 + 8;
-     }
+    y = -50;
+    x = 76;
+    for (var i = 0; i < 14; i++) {
+        creation.createBox([x, y, 44], [10, 7.5, 65], ['texture_wall2'], scene);
+        y = y + 7.5;
+        x = x + 8;
+    }
 
 
 
-/**********************
-    Camera movement
-************************/
+    /**********************
+        Camera movement
+    ************************/
 
 }
 
@@ -163,15 +181,55 @@ function onWindowResize() {
 
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
 
 function animate() {
 
-    requestAnimationFrame( animate );
-    controls.update();
+    requestAnimationFrame(animate);
 
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
+}
 
+
+function onClick(event) {
+    // Calculates mouse position in normalized device coordinates
+    // Allow to have a value between -1 & 1
+
+    click.x = (event.clientX / window.innerWidth) * 2 - 1;
+    click.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera( click, camera);
+    const found = raycaster.intersectObjects( scene.children, true);
+    if(found.length > 0 && found[0].object.userData.draggable == true) {
+        checkAnimation(found[0].object.userData.name);
+    }
+}
+
+function checkAnimation(name) {
+    switch(name) {
+        case "bridge":
+            let counter;
+            if(elements[name].userData.status == "down") {
+                elements[name].userData.status = "up";
+                counter = 0;
+                let open = setInterval(function(){
+                    elements['bridge'].rotation.x = counter * 0.01;
+                    if(elements['bridge'].rotation.x == 1.5) clearInterval(open);
+                    counter++;
+                }, 10)
+            } else {
+                elements[name].userData.status = "down";
+                counter = 150;
+                let open = setInterval(function(){
+                    elements['bridge'].rotation.x = counter * 0.01;
+                    if(elements['bridge'].rotation.x == 0) clearInterval(open);
+                    counter--;
+                }, 10)
+            }
+            break;
+        default:
+            console.log("Nothing found");
+    }
 }
